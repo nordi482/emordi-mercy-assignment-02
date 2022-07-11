7b93b452b0f316dec140707a8c4a6c0bf3a7c4de
Emordi Mercy <78581647+nordi482@users.noreply.github.com>
2022-07-11 17:49:38 +0000

    finished challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sumNumbers = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++ ) {
+        sumNumbers += arrayOfNumbers[i]
+  }
+    return sumNumbers;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
